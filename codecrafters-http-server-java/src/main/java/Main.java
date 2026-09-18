b8e5561425114ef57fb68e4b2c6e32377b92b48d
Jitendra Singh <jitendra.singh.sola@gmail.com>
2026-09-18 19:17:02 +0530

    Implement Stage 2: Respond with 200

--- codecrafters-http-server-java/src/main/java/Main.java
+++ codecrafters-http-server-java/src/main/java/Main.java
@@ -1,11 +1,19 @@
 import java.io.IOException;
+import java.io.OutputStream;
 import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 
 public class Main {
   public static void main(String[] args) {
     try (ServerSocket serverSocket = new ServerSocket(4221)) {
       serverSocket.setReuseAddress(true);
-      serverSocket.accept();
+      try (Socket clientSocket = serverSocket.accept()) {
+        OutputStream out = clientSocket.getOutputStream();
+        String response = "HTTP/1.1 200 OK\r\n\r\n";
+        out.write(response.getBytes(StandardCharsets.UTF_8));
+        out.flush();
+      }
     } catch (IOException e) {
       System.err.println("IOException: " + e.getMessage());
     }
