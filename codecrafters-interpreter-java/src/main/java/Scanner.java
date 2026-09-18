a176e3c9401a7ea158b7e575ca27002b4f50a4af
Jitendra Singh <jitendra.singh.sola@gmail.com>
2026-09-18 20:11:29 +0530

    Solve Stage 13: Scanning - Number literals

--- codecrafters-interpreter-java/src/main/java/Scanner.java
+++ codecrafters-interpreter-java/src/main/java/Scanner.java
@@ -92,11 +92,29 @@ public class Scanner {
                 string();
                 break;
             default:
-                // Lexical error: unexpected character encountered
-                System.err.println("[line " + line + "] Error: Unexpected character: " + c);
-                hasError = true;
+                if (isDigit(c)) {
+                    number();
+                } else {
+                    // Lexical error: unexpected character encountered
+                    System.err.println("[line " + line + "] Error: Unexpected character: " + c);
+                    hasError = true;
+                }
                 break;
         }
+    }
+
+    private void number() {
+        while (isDigit(peek())) advance();
+
+        // Look for a fractional part.
+        if (peek() == '.' && isDigit(peekNext())) {
+            // Consume the "."
+            advance();
+
+            while (isDigit(peek())) advance();
+        }
+
+        addToken(TokenType.NUMBER, Double.parseDouble(source.substring(start, current)));
     }
 
     private void string() {
@@ -130,6 +148,15 @@ public class Scanner {
     private char peek() {
         if (isAtEnd()) return '\0';
         return source.charAt(current);
+    }
+
+    private char peekNext() {
+        if (current + 1 >= source.length()) return '\0';
+        return source.charAt(current + 1);
+    }
+
+    private boolean isDigit(char c) {
+        return c >= '0' && c <= '9';
     }
 
     private boolean isAtEnd() {
