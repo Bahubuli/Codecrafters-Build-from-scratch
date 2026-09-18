0e5f6f28b853b127d647c6d0ced92acc2ba2b877
Jitendra Singh <jitendra.singh.sola@gmail.com>
2026-09-18 19:42:58 +0530

    Solve Stage 1: Decode bencoded strings

--- codecrafters-bittorrent-java/src/main/java/Main.java
+++ codecrafters-bittorrent-java/src/main/java/Main.java
@@ -1,46 +1,40 @@
 import com.google.gson.Gson;
-// import com.dampcake.bencode.Bencode; - available if you need it!
 
 public class Main {
   private static final Gson gson = new Gson();
 
   public static void main(String[] args) throws Exception {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    System.err.println("Logs from your program will appear here!");
-    
-    String command = args[0];
-    if("decode".equals(command)) {
-      //  TODO: Uncomment the code below to pass the first stage
-      //  String bencodedValue = args[1];
-      //  String decoded;
-      //  try {
-      //    decoded = decodeBencode(bencodedValue);
-      //  } catch(RuntimeException e) {
-      //    System.out.println(e.getMessage());
-      //    return;
-      //  }
-      //  System.out.println(gson.toJson(decoded));
+    if (args.length < 2) {
+      System.err.println("Usage: your_program.sh <command> <args>");
+      System.exit(1);
+    }
 
+    String command = args[0];
+    if ("decode".equals(command)) {
+      String bencodedValue = args[1];
+      Object decoded;
+      try {
+        decoded = decodeBencode(bencodedValue);
+      } catch (RuntimeException e) {
+        System.out.println(e.getMessage());
+        return;
+      }
+      System.out.println(gson.toJson(decoded));
     } else {
       System.out.println("Unknown command: " + command);
     }
-
   }
 
-  static String decodeBencode(String bencodedString) {
+  static Object decodeBencode(String bencodedString) {
     if (Character.isDigit(bencodedString.charAt(0))) {
-      int firstColonIndex = 0;
-      for(int i = 0; i < bencodedString.length(); i++) { 
-        if(bencodedString.charAt(i) == ':') {
-          firstColonIndex = i;
-          break;
-        }
+      int firstColonIndex = bencodedString.indexOf(':');
+      if (firstColonIndex == -1) {
+        throw new RuntimeException("Invalid bencoded string: missing colon");
       }
       int length = Integer.parseInt(bencodedString.substring(0, firstColonIndex));
-      return bencodedString.substring(firstColonIndex+1, firstColonIndex+1+length);
+      return bencodedString.substring(firstColonIndex + 1, firstColonIndex + 1 + length);
     } else {
       throw new RuntimeException("Only strings are supported at the moment");
     }
   }
-  
 }
