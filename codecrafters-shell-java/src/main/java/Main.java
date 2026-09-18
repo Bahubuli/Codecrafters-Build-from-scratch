054e95a9dff2f6e4ec4f292f555036a54e890895
Jitendra Singh <jitendra.singh.sola@gmail.com>
2026-09-18 09:54:26 +0530

    Solve Stage 3: Implement a REPL

--- codecrafters-shell-java/src/main/java/Main.java
+++ codecrafters-shell-java/src/main/java/Main.java
@@ -2,10 +2,15 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.print("$ ");
-
         Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
-        System.out.println(input + ": command not found");
+
+        while (true) {
+            System.out.print("$ ");
+            if (!scanner.hasNextLine()) {
+                break;
+            }
+            String input = scanner.nextLine();
+            System.out.println(input + ": command not found");
+        }
     }
 }
