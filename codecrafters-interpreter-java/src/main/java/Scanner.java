69311b8ce81a3928356ea61526c5a3b57d06b460
Jitendra Singh <jitendra.singh.sola@gmail.com>
2026-09-18 19:46:10 +0530

    Solve Stage 03: Scanning - Braces

--- codecrafters-interpreter-java/src/main/java/Scanner.java
+++ codecrafters-interpreter-java/src/main/java/Scanner.java
@@ -36,6 +36,12 @@ public class Scanner {
             case ')':
                 addToken(TokenType.RIGHT_PAREN);
                 break;
+            case '{':
+                addToken(TokenType.LEFT_BRACE);
+                break;
+            case '}':
+                addToken(TokenType.RIGHT_BRACE);
+                break;
             case ' ':
             case '\r':
             case '\t':
