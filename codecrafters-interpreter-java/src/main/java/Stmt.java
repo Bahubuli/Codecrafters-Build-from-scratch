06faa45c6e814868ce64c06764035624d006faa4
Jitendra Singh <jitendra.singh.sola@gmail.com>
2026-09-19 02:02:03 +0530

    Solve Interpreter Stage 59: Functions without arguments

--- codecrafters-interpreter-java/src/main/java/Stmt.java
+++ codecrafters-interpreter-java/src/main/java/Stmt.java
@@ -4,6 +4,7 @@ public abstract class Stmt {
     public interface Visitor<R> {
         R visitBlockStmt(Block stmt);
         R visitExpressionStmt(Expression stmt);
+        R visitFunctionStmt(Function stmt);
         R visitIfStmt(If stmt);
         R visitPrintStmt(Print stmt);
         R visitVarStmt(Var stmt);
@@ -35,6 +36,23 @@ public abstract class Stmt {
         @Override
         public <R> R accept(Visitor<R> visitor) {
             return visitor.visitExpressionStmt(this);
+        }
+    }
+
+    public static class Function extends Stmt {
+        public final Token name;
+        public final List<Token> params;
+        public final List<Stmt> body;
+
+        public Function(Token name, List<Token> params, List<Stmt> body) {
+            this.name = name;
+            this.params = params;
+            this.body = body;
+        }
+
+        @Override
+        public <R> R accept(Visitor<R> visitor) {
+            return visitor.visitFunctionStmt(this);
         }
     }
 
