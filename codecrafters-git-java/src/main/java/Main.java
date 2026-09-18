5a7ee974977e8a206b75c2140b2b23882bbeaf33
Jitendra Singh <jitendra.singh.sola@gmail.com>
2026-09-19 01:04:44 +0530

    Solve Git Stage 2: Read a blob object

--- codecrafters-git-java/src/main/java/Main.java
+++ codecrafters-git-java/src/main/java/Main.java
@@ -1,9 +1,13 @@
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Files;
+import java.util.zip.InflaterInputStream;
 
 public class Main {
-  public static void main(String[] args){
+  public static void main(String[] args) {
     if (args.length == 0) {
       System.err.println("Usage: ./your_program.sh <command> [<args>]");
       System.exit(1);
@@ -23,6 +27,36 @@ public class Main {
           System.out.println("Initialized git directory");
         } catch (IOException e) {
           throw new RuntimeException(e);
+        }
+      }
+      case "cat-file" -> {
+        if (args.length >= 3 && args[1].equals("-p")) {
+          String sha = args[2];
+          String dir = sha.substring(0, 2);
+          String filename = sha.substring(2);
+          File objectFile = new File(".git/objects/" + dir + "/" + filename);
+          try (InputStream in = new InflaterInputStream(new FileInputStream(objectFile));
+               ByteArrayOutputStream out = new ByteArrayOutputStream()) {
+            byte[] buf = new byte[8192];
+            int read;
+            while ((read = in.read(buf)) != -1) {
+              out.write(buf, 0, read);
+            }
+            byte[] decompressed = out.toByteArray();
+            int nullIndex = -1;
+            for (int i = 0; i < decompressed.length; i++) {
+              if (decompressed[i] == 0) {
+                nullIndex = i;
+                break;
+              }
+            }
+            if (nullIndex != -1) {
+              System.out.write(decompressed, nullIndex + 1, decompressed.length - (nullIndex + 1));
+              System.out.flush();
+            }
+          } catch (IOException e) {
+            throw new RuntimeException(e);
+          }
         }
       }
       default -> System.out.println("Unknown command: " + command);
