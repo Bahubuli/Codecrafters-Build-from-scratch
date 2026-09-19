c6d95f702835112735b2c51eb26e516cb3b5689d
Jitendra Singh <jitendra.singh.sola@gmail.com>
2026-09-19 09:51:54 +0530

    Solve Interpreter Stage 73: Getters & Setters

--- codecrafters-interpreter-java/src/main/java/Expr.java
+++ codecrafters-interpreter-java/src/main/java/Expr.java
@@ -5,9 +5,11 @@ public abstract class Expr {
         R visitAssignExpr(Assign expr);
         R visitBinaryExpr(Binary expr);
         R visitCallExpr(Call expr);
+        R visitGetExpr(Get expr);
         R visitGroupingExpr(Grouping expr);
         R visitLiteralExpr(Literal expr);
         R visitLogicalExpr(Logical expr);
+        R visitSetExpr(Set expr);
         R visitUnaryExpr(Unary expr);
         R visitVariableExpr(Variable expr);
     }
@@ -63,6 +65,21 @@ public abstract class Expr {
         }
     }
 
+    public static class Get extends Expr {
+        public final Expr object;
+        public final Token name;
+
+        public Get(Expr object, Token name) {
+            this.object = object;
+            this.name = name;
+        }
+
+        @Override
+        public <R> R accept(Visitor<R> visitor) {
+            return visitor.visitGetExpr(this);
+        }
+    }
+
     public static class Grouping extends Expr {
         public final Expr expression;
 
@@ -103,6 +120,23 @@ public abstract class Expr {
         @Override
         public <R> R accept(Visitor<R> visitor) {
             return visitor.visitLogicalExpr(this);
+        }
+    }
+
+    public static class Set extends Expr {
+        public final Expr object;
+        public final Token name;
+        public final Expr value;
+
+        public Set(Expr object, Token name, Expr value) {
+            this.object = object;
+            this.name = name;
+            this.value = value;
+        }
+
+        @Override
+        public <R> R accept(Visitor<R> visitor) {
+            return visitor.visitSetExpr(this);
         }
     }
 
